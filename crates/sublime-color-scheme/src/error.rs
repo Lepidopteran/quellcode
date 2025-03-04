5876e102d1a1437d45af58e2931cfee061873011
Blaine <blainenicholls@proton.me>
2025-03-03 19:40:09 -0500

    chore(sublime-color-scheme): organize errors

--- crates/sublime-color-scheme/src/error.rs
+++ crates/sublime-color-scheme/src/error.rs
@@ -3,38 +3,47 @@ use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum ParseError {
+    // File and JSON Parsing Errors
+    #[error("Failed to open file")]
+    OpenFile(#[from] std::io::Error),
     #[error("Failed to parse JSON")]
     Json(#[from] serde_json::Error),
     #[error("Failed to parse JSONC")]
     Jsonc(#[from] jsonc_parser::errors::ParseError),
-    #[error("Failed to convert color")]
-    ConvertColor(String),
+
+    // Color Parsing Errors
     #[error("Failed to parse hex color")]
     InvalidHexColor,
     #[error("Failed to convert keyword to color")]
     InvalidKeyword,
-    #[error("Failed to parse variable function")]
-    InvalidVariable,
     #[error("Not a valid color function")]
     InvalidColorFunction,
-    #[error("Failed to parse adjuster")]
-    ParseAdjuster,
-    #[error("Failed to parse number string: {0}")]
-    InvalidNumber(String),
-    #[error("Failed to parse function")]
-    ParseFunction,
     #[error("Failed to parse color space")]
     ParseColorSpace,
-    #[error("Failed to parse scope")]
-    ParseScope(#[from] ParseScopeError),
+
+    // Function and Expression Parsing Errors
+    #[error("Failed to parse variable function")]
+    InvalidVariable,
+    #[error("Failed to parse adjuster")]
+    ParseAdjuster,
+    #[error("Failed to parse function")]
+    ParseFunction,
     #[error("Failed to parse expression")]
     ParseExpression,
-    #[error("Failed to parse theme")]
-    ParseTheme(#[from] ParseThemeError),
+    #[error("Could not find variable")]
+    UnknownVariable,
+
+    // Number Parsing Errors
+    #[error("Failed to parse number string: {0}")]
+    InvalidNumber(String),
     #[error("Failed to parse float number")]
     ParseNumber(#[from] std::num::ParseFloatError),
     #[error("Failed to parse integer number")]
     ParseInteger(#[from] std::num::ParseIntError),
-    #[error("Could not find variable")]
-    UnknownVariable,
+
+    // Scope and Theme Parsing Errors
+    #[error("Failed to parse scope")]
+    ParseScope(#[from] ParseScopeError),
+    #[error("Failed to parse theme")]
+    ParseTheme(#[from] ParseThemeError),
 }
