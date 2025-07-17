d1b5c4b435759d2c462908db9b26c7241f168f54
Blaine <blainenicholls@proton.me>
2025-07-17 16:11:17 -0400

    refactor(asset): use glib::Enum instead of repr u8 for AssetType

--- quellcode/src/app/asset_store/asset/object.rs
+++ quellcode/src/app/asset_store/asset/object.rs
@@ -6,25 +6,16 @@ use crate::app::scraping::package_control::Entry;
 #[cfg(test)]
 use super::init;
 
-#[derive(Debug, Clone, Default, PartialEq, Eq, serde::Serialize, serde::Deserialize)]
-#[repr(u8)]
+#[derive(
+    Debug, Copy, Clone, Default, PartialEq, Eq, glib::Enum, serde::Serialize, serde::Deserialize,
+)]
+#[enum_type(name = "QuellcodeAssetType")]
 pub enum AssetType {
     #[default]
     Unknown,
     ColorScheme,
     LanguageSyntax,
     VSCodeTheme,
-}
-
-impl From<u8> for AssetType {
-    fn from(value: u8) -> Self {
-        match value {
-            1 => Self::ColorScheme,
-            2 => Self::LanguageSyntax,
-            3 => Self::VSCodeTheme,
-            _ => Self::Unknown,
-        }
-    }
 }
 
 #[derive(Default, Clone, Debug, serde::Serialize, serde::Deserialize)]
@@ -97,7 +88,7 @@ mod imp {
         #[property(name = "installs", get, set, member = installs, type = i64)]
         #[property(name = "source", get, set, member = source, type = String)]
         #[property(name = "url", get, set, member = url, type = String)]
-        #[property(name = "kind", get = |a: &Asset| a.data.borrow().kind.clone() as u8, set = |a: &Asset, v: u8| a.data.borrow_mut().kind = v.into(), type = u8)]
+        #[property(name = "kind", get, set, member = kind, type = AssetType, builder(AssetType::Unknown))]
         pub data: RefCell<AssetData>,
     }
 
@@ -131,30 +122,5 @@ impl Asset {
 impl From<AssetData> for Asset {
     fn from(data: AssetData) -> Self {
         Asset::new(data)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_asset_type_from_u8() {
-        init();
-
-        assert_eq!(AssetType::from(1), AssetType::ColorScheme);
-        assert_eq!(AssetType::from(2), AssetType::LanguageSyntax);
-        assert_eq!(AssetType::from(3), AssetType::VSCodeTheme);
-        assert_eq!(AssetType::from(4), AssetType::Unknown);
-    }
-
-    #[test]
-    fn test_asset_type_to_u8() {
-        init();
-
-        assert_eq!(AssetType::Unknown as u8, 0);
-        assert_eq!(AssetType::ColorScheme as u8, 1);
-        assert_eq!(AssetType::LanguageSyntax as u8, 2);
-        assert_eq!(AssetType::VSCodeTheme as u8, 3);
     }
 }
