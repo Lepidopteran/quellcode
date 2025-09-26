7b5d7bca6188a4d4b2ae4aeccd6b9d07da9ef4d1
Blaine <blainenicholls@proton.me>
2025-09-25 21:48:13 -0400

    feat(frontend): get basic shape of layout

--- quellcode/frontend/src/App.svelte
+++ quellcode/frontend/src/App.svelte
@@ -2,8 +2,31 @@
 	import { invoke } from "@tauri-apps/api/core";
 	import Combobox from "@components/input/Combobox.svelte";
 	import FontSelector from "@components/input/FontSelector.svelte";
+
+	let fontFamily: string | null = $state(null);
 </script>
 
-<main class="h-svh">
-	<FontSelector defaultFamily="JetBrains Mono" />
-</main>
+<div class="h-svh grid grid-cols-[1fr_300px]">
+	<main class="p-2 h-full bg-base-200">
+		<div class="h-full flex flex-col items-stretch justify-items-stretch gap-2">
+			<div
+				contenteditable="true"
+				class="font-mono border border-black/50 shadow-md h-full rounded-theme"
+				style:font-family={fontFamily}
+			></div>
+
+			<div
+				class="font-mono border border-black/50 shadow-md h-full rounded-theme"
+				style:font-family={fontFamily}
+			></div>
+		</div>
+	</main>
+	<aside class="p-2 overflow-y-auto h-full bg-base-100">
+		<details class="w-full bg-base-200/50 px-2 rounded-theme">
+			<summary class="font-bold select-none py-2">Code Settings</summary>
+			<div>
+				<FontSelector class="w-full" onChange={(f) => (fontFamily = f)} defaultFamily="JetBrains Mono" />
+			</div>
+		</details>
+	</aside>
+</div>
